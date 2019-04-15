de4100bcdbc673fa9f2d2c608cbbd8754b3ca8f0
Michael Lin <unadlib@gmail.com>
2019-04-15 23:44:22 +0800

    add redux logger middleware

--- src/lib/baseModule.js
+++ src/lib/baseModule.js
@@ -1,6 +1,15 @@
+import { applyMiddleware, createStore } from 'redux';
+import logger from 'redux-logger';
 import Module, { state, action, computed } from 'usm-redux';
 
-class BaseModule extends Module {}
+class BaseModule extends Module {
+  static _generateStore(_, { reducers }) {
+    return createStore(
+      reducers,
+      applyMiddleware(logger)
+    );
+  }
+}
 
 export {
   BaseModule as default,
