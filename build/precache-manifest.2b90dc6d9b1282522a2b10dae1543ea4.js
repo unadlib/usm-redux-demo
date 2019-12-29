1dc1c552e82a7846950d535996ef44b952db6371
Michael Lin <unadlib@gmail.com>
2019-12-30 02:27:50 +0800

    update usm-redux v1.3.7

---
+++ build/precache-manifest.2b90dc6d9b1282522a2b10dae1543ea4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "eef55e25a3b6bfeafc4e",
+    "url": "/static/js/main.54fa4d27.chunk.js"
+  },
+  {
+    "revision": "861e9ae7b30fd749fa99",
+    "url": "/static/js/2.3805dc2d.chunk.js"
+  },
+  {
+    "revision": "cbb117d7dac1530a37abe4eb5405d418",
+    "url": "/index.html"
+  }
+];
